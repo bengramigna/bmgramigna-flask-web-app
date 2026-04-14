c37c70c5c3586248b5660d560832e71f3532e57c
Ben Gramigna <ben-gramigna@uiowa.edu>
2026-04-14 14:18:32 -0500

    final changes

---
+++ app.py
@@ -0,0 +1,22 @@
+# module 11 - Flask Application
+# Mike Colbert 03/31/2026
+
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template("index.html")
+
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")    
+
+
+
+
+if __name__ == "__main__":
+      app.run(host="0.0.0.0", port=5002, debug=True)
